aa80585027c9f932e2f028a422e90095a81568ef
trip-harsh <138213931+trip-harsh@users.noreply.github.com>
2023-07-09 15:53:00 +0530

    Update assignment_1.sql

--- assignment_1.sql
+++ assignment_1.sql
@@ -64,58 +64,72 @@ CREATE TABLE dependents(
 
 -- Question 2:-
 
-INSERT INTO jobs VALUES (1, "Software Developer", 700000, 2000000);
-INSERT INTO jobs VALUES (2, "Web Developer", 500000, 1500000);
-INSERT INTO jobs VALUES (3, "Cloud Developer", 800000, 2000000);
-INSERT INTO jobs VALUES (4, "AI Developer", 700000, 2000000);
-INSERT INTO jobs VALUES (5, "Network Architect", 1200000, 2200000);
+INSERT INTO jobs 
+VALUES 
+(1, "Software Developer", 700000, 2000000),
+(2, "Web Developer", 500000, 1500000),
+(3, "Cloud Developer", 800000, 2000000),
+(4, "AI Developer", 700000, 2000000),
+(5, "Network Architect", 1200000, 2200000);
 
 
-INSERT INTO regions VALUES (1, "Asia");
-INSERT INTO regions VALUES (2, "Euorpe");
-INSERT INTO regions VALUES (3, "North America");
-INSERT INTO regions VALUES (4, "Africa");
-INSERT INTO regions VALUES (5, "South America");
+INSERT INTO regions 
+VALUES 
+  (1, "Asia"),
+  (2, "Euorpe"),
+  (3, "North America"),
+  (4, "Africa"),
+  (5, "South America");
 
 
-INSERT INTO countries VALUES (1, "India", 1);
-INSERT INTO countries VALUES (2, "China", 1);
-INSERT INTO countries VALUES (3, "America", 3);
-INSERT INTO countries VALUES (4, "Spain", 5);
-INSERT INTO countries VALUES (5, "Belgium", 2);
+INSERT INTO countries 
+VALUES 
+  (1, "India", 1),
+  (2, "China", 1),
+  (3, "America", 3),
+  (4, "Spain", 5),
+  (5, "Belgium", 2);
 
 
-INSERT INTO locations VALUES (1, "abc street", "12345", "Bhopal", "Madhya Pradesh", 1);
-INSERT INTO locations VALUES (2, "def street", "35675", "Cicero", "Illinois(IL)", 3);
-INSERT INTO locations VALUES (3, "ghi street", "43234", "Madrid", "Spain", 4);
-INSERT INTO locations VALUES (4, "jkl street", "34353", "Barcelona", "Spain", 4);
-INSERT INTO locations VALUES (5, "mno street", "46323", "Sevilla", "Spain", 4);
+INSERT INTO locations 
+VALUES 
+  (1, "abc street", "12345", "Bhopal", "Madhya Pradesh", 1),
+  (2, "def street", "35675", "Cicero", "Illinois(IL)", 3),
+  (3, "ghi street", "43234", "Madrid", "Spain", 4),
+  (4, "jkl street", "34353", "Barcelona", "Spain", 4),
+  (5, "mno street", "46323", "Sevilla", "Spain", 4);
 
 
-INSERT INTO departments VALUES (1, "IT", 1);
-INSERT INTO departments VALUES (2, "IT", 2);
-INSERT INTO departments VALUES (3, "IT", 3);
-INSERT INTO departments VALUES (4, "HR", 1);
-INSERT INTO departments VALUES (5, "HR", 2);
+INSERT INTO departments 
+VALUES 
+  (1, "IT", 1),
+  (2, "IT", 2),
+  (3, "IT", 3),
+  (4, "HR", 1),
+  (5, "HR", 2);
 
 
-INSERT INTO employees VALUES (1, "Harsh", "Tripathi", "abc@gmail.com", "2023-05-15", 5, 70000, 1, 3);
-INSERT INTO employees VALUES (2, "Saket", "Patel", "bcd@gmail.com", "2022-05-15", 1, 40000, 1, 3);
-INSERT INTO employees VALUES (3, "Shubh", "Agrawal", "cde@gmail.com", "2021-05-15", 1, 55000, 2, 3);
-INSERT INTO employees VALUES (4, "Vineet", "Tiwari", "efg@gmail.com", "2022-05-15", 2, 45000, 1, 3);
-INSERT INTO employees VALUES (5, "Diksha", "Sharma", "hij@gmail.com", "2020-05-15", 1, 43000, 1, 3);
-INSERT INTO employees VALUES (6, "Sourabh", "Verma", "klm@gmail.com", "2022-06-15", 4, 34000, 1, 4);
-INSERT INTO employees VALUES (7, "Priya", "Patel", "nop@gmail.com", "2021-03-15", 3, 35000, 1, 4);
-INSERT INTO employees VALUES (8, "Somya", "Jain", "qrs@gmail.com", "2020-04-15", 2, 45000, 1, 4);
-INSERT INTO employees VALUES (9, "Palak", "Yadav", "tuv@gmail.com", "2022-05-15", 1, 34000, 1, 2);
-INSERT INTO employees VALUES (10, "Lokesh", "Chaudary", "wxy@gmail.com", "2021-08-15", 4, 1500000, 1, 3);
+INSERT INTO employees 
+VALUES 
+  (1, "Harsh", "Tripathi", "abc@gmail.com", "2023-05-15", 5, 70000, 1, 3),
+  (2, "Saket", "Patel", "bcd@gmail.com", "2022-05-15", 1, 40000, 1, 3),
+  (3, "Shubh", "Agrawal", "cde@gmail.com", "2021-05-15", 1, 55000, 2, 3),
+  (4, "Vineet", "Tiwari", "efg@gmail.com", "2022-05-15", 2, 45000, 1, 3),
+  (5, "Diksha", "Sharma", "hij@gmail.com", "2020-05-15", 1, 43000, 1, 3),
+  (6, "Sourabh", "Verma", "klm@gmail.com", "2022-06-15", 4, 34000, 1, 4),
+  (7, "Priya", "Patel", "nop@gmail.com", "2021-03-15", 3, 35000, 1, 4),
+  (8, "Somya", "Jain", "qrs@gmail.com", "2020-04-15", 2, 45000, 1, 4),
+  (9, "Palak", "Yadav", "tuv@gmail.com", "2022-05-15", 1, 34000, 1, 2),
+  (10, "Lokesh", "Chaudary", "wxy@gmail.com", "2021-08-15", 4, 1500000, 1, 3);
 
 
-INSERT INTO dependents VALUES (1, "Rushil", "Tripathi", "Brother", 1);
-INSERT INTO dependents VALUES (2, "Jim", "Moriarty", "Brother", 2);
-INSERT INTO dependents VALUES (3, "Jim", "Moriarty", "Brother", 3);
-INSERT INTO dependents VALUES (4, "Jim", "Moriarty", "Brother", 4);
-INSERT INTO dependents VALUES (5, "Jim", "Moriarty", "Brother", 5);
+INSERT INTO dependents 
+VALUES 
+  (1, "Rushil", "Tripathi", "Brother", 1),
+  (2, "Jim", "Moriarty", "Brother", 2),
+  (3, "Jim", "Moriarty", "Brother", 3),
+  (4, "Jim", "Moriarty", "Brother", 4),
+  (5, "Jim", "Moriarty", "Brother", 5);
 
 -- Question 3:-
 
